012196c06f9be9a8ae1598258f6159482016adde
VenomouSTaCo <taco.v@yandex.ru>
2023-03-22 22:41:59 +0500

    [feat] add rec card layout

---
+++ vue-Steam-Better-RecSys/src/views/RecommendationView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper d-flex justify-center align-center flex-column">
+    <recommendation-card/>
+    <v-btn class="mt-7" @click="$router.push({path: '/'})">Get back</v-btn>
+  </div>
+</template>
+
+<script>
+import RecommendationCard from "@/components/RecommendationCard.vue";
+export default {
+components:{RecommendationCard}
+}
+</script>
+
+<style scoped>
+.wrapper{
+  width: 100%;
+  height: 100%;
+  background: rgb(43,53,68);
+}
+</style>
