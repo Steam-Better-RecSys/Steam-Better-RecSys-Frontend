b1fc37f3ee0b764656f1a3ea5d46f9abb2e58ee6
Ant <taco.v@yandex.ru>
2023-03-28 16:52:57 +0300

    [fix] new margins and height

--- vue-Steam-Better-RecSys/src/components/SearchHeader.vue
+++ vue-Steam-Better-RecSys/src/components/SearchHeader.vue
@@ -1,20 +1,16 @@
 <template>
-  <v-app-bar class="pa-5" style="height: 10%; background: rgb(43,53,68); color: white">
-    <v-app-bar-title>
-      <v-text-field class="w-25 font-weight-black" label="search" variant="underlined"></v-text-field>
-    </v-app-bar-title>
-  </v-app-bar>
+  <div class="head-wrapper">
+    <v-app-bar class="pa-5" style="height: 100px; background: rgb(43,53,68); color: white">
+      <v-app-bar-title>
+        <v-text-field class="w-25 font-weight-black" label="search" variant="underlined"></v-text-field>
+      </v-app-bar-title>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
-import FiltersMenu from "@/components/FiltersMenu.vue";
-export default {
-  components: {
-    FiltersMenu
-  }
-}
+export default {}
 </script>
 
 <style scoped>
-
 </style>
