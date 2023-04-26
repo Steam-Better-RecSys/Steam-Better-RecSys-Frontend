4c686e9b924c26f73440b250371813ce691675c4
eispoohw <darina.kormanovskaya@gmail.com>
2023-04-26 22:21:42 +0300

    [feat] add filters and main card

--- vue-Steam-Better-RecSys/src/components/MainCard.vue
+++ vue-Steam-Better-RecSys/src/components/MainCard.vue
@@ -1,19 +1,90 @@
 <template>
-<div class="overflow-y-auto d-flex justify-center align-center">
-  <div class="h-75 w-75 bg-amber d-flex justify-center align-center">
-    <h1>Wait for backend and next sprint</h1>
-  </div>
-</div>
+    <div class="">
+        <div class="main-wrap w-100 h-100 pa-8 d-flex flex-column align-center">
+            <div class="grid-container">
+                <div class="game-wrap" v-for="game in games">
+                    <img class="game-img" :src="game.verticalImageUrl" alt="" :id="game.id"
+                         @click="select(game.id)" :class="{active: game.isSelected}">
+                </div>
+            </div>
+            <Pagination/>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import {mapActions} from "pinia";
+import useGamesStore from "@/stores/games";
+import Pagination from "@/components/UI/Pagination.vue";
 
+export default {
+    name: "mainCard",
+    components: {
+        Pagination
+    },
+    data() {
+        return {
+            games: [],
+        }
+    },
+    methods: {
+        ...mapActions(useGamesStore, ['getAllGamesStore']),
+        async render() {
+            this.games = await this.getAllGamesStore();
+            for (let game in this.games) {
+                this.games[game].isSelected = false;
+            }
+
+        },
+        select(id) {
+            this.games.find(game => game.id === id).isSelected = !this.games.find(game => game.id === id).isSelected
+
+        }
+
+    },
+    mounted() {
+        this.render()
+    }
 }
 </script>
 
 <style scoped>
-div{
-  height: 700px;
+div {
+    height: max-content;
+}
+
+.grid-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    row-gap: 50px;
+    margin-bottom: 30px;
+}
+
+.game-wrap {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    height: max-content;
+}
+
+
+.game-img {
+    width: 65%;
+    display: flex;
+    border-radius: 15px;
+    opacity: 0.7;
+}
+
+.game-img.active {
+    opacity: 1;
+    -webkit-box-shadow: 0 0 10px #fff;
+    box-shadow: 0 0 10px #fff;
+}
+
+.game-img:hover {
+    transform: scale(1.03);
+    transition: 0.3s;
+    cursor: pointer;
 }
 </style>
---
+++ vue-Steam-Better-RecSys/src/components/UI/Pagination.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="pagination d-lg-inline-flex">
+        <a href="#">&laquo;</a>
+        <a href="#" class="active">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+        <a href="#">6</a>
+        <a href="#">&raquo;</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Pagination"
+
+}
+</script>
+
+<style scoped>
+a{
+    color: white;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+
+a.active{
+    background-color: #6e4bc2;
+    color: white;
+    border-radius: 5px;
+}
+
+a:hover:not(.active) {
+    background-color: #1B2228FF;
+    border-radius: 5px;
+}
+</style>
