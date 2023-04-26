ea2c46b1a0760a9e82831f3ab27a7e5f369363c7
eispoohw <darina.kormanovskaya@gmail.com>
2023-04-26 22:13:12 +0300

    [feat] add api and store for tags

---
+++ vue-Steam-Better-RecSys/src/stores/tags.js
@@ -0,0 +1,18 @@
+import {defineStore} from 'pinia'
+import {tagsAPI} from "@/api";
+
+const useTagsStore = defineStore('tags', {
+    state: () => ({
+        classes: []
+    }),
+    actions: {
+        async getAllTagsStore() {
+            const response = await tagsAPI.getAllTags()
+            this.classes = response.data
+
+            return this.classes
+        },
+    }
+})
+
+export default useTagsStore
