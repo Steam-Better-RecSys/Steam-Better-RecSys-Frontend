ceedc0ce2645346091adc61951db254d26d880aa
eispoohw <darina.kormanovskaya@gmail.com>
2023-04-26 22:20:39 +0300

    [feat] added bootstrap

--- vue-Steam-Better-RecSys/src/main.js
+++ vue-Steam-Better-RecSys/src/main.js
@@ -6,7 +6,7 @@ import store from './stores'
 
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import {far,faThumbsUp, faThumbsDown, faHeart} from "@fortawesome/free-regular-svg-icons";
+import {far, faThumbsUp, faThumbsDown, faHeart} from "@fortawesome/free-regular-svg-icons";
 import {faS, faBan} from "@fortawesome/free-solid-svg-icons";
 import {faForward, faBackward} from "@fortawesome/free-solid-svg-icons";
 
@@ -15,6 +15,10 @@ import {createVuetify} from 'vuetify'
 import {aliases, fa} from 'vuetify/iconsets/fa-svg'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 
 const vuetify = createVuetify({
     icons: {
@@ -36,5 +40,4 @@ app.use(store)
 app.use(router)
 app.use(vuetify)
 app.component('font-awesome-icon', FontAwesomeIcon)
-
 app.mount('#app')
