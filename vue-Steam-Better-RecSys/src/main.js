45416c728cbc59785b15ff7898d8c395856fa0c6
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-05-02 00:14:23 +0300

    :fire: [remove] vuetify at all

--- vue-Steam-Better-RecSys/src/main.js
+++ vue-Steam-Better-RecSys/src/main.js
@@ -6,7 +6,6 @@ import store from './stores';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { far } from '@fortawesome/free-regular-svg-icons';
 import {
     faS,
     faBan,
@@ -14,46 +13,25 @@ import {
     faThumbsUp,
     faThumbsDown,
     faHeart,
+    faForward
 } from '@fortawesome/free-solid-svg-icons';
-import { faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
-
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import { aliases, fa } from 'vuetify/iconsets/fa-svg';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-
-const vuetify = createVuetify({
-    icons: {
-        defaultSet: 'fa',
-        aliases,
-        sets: {
-            fa,
-        },
-    },
-    components,
-    directives,
-});
 
 const app = createApp(App);
 
 library.add(
     faS,
-    far,
     faThumbsUp,
     faThumbsDown,
     faBan,
     faHeart,
     faForward,
-    faBackward,
     faWandMagic
 );
 
 app.use(store);
 app.use(router);
-app.use(vuetify);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
