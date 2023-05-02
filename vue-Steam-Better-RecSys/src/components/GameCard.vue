5bd3303824a2755265f6b7143c18ca37a2022f5e
eispoohw <darina.kormanovskaya@gmail.com>
2023-05-02 09:32:00 +0300

    :hammer: [refactor] move game card to separate component

---
+++ vue-Steam-Better-RecSys/src/components/GameCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <img
+        class="game-img"
+        :src="image"
+        :alt="title"
+        :id="id"
+        @click="setActive(id)"
+        :class="{ active: isActive }"
+        @error="setImage($event.currentTarget)"
+    />
+</template>
+
+<script>
+export default {
+    name: 'GameCard',
+    data() {
+        return {
+            isActive: false,
+        };
+    },
+    props: {
+        title: {
+            type: String,
+        },
+        image: {
+            type: String,
+        },
+        id: {
+            type: Number,
+        },
+    },
+    methods: {
+        setActive(id) {
+            this.isActive = !this.isActive;
+        },
+        setImage(image) {
+            image.setAttribute('src', 'src/assets/Image404.png');
+        },
+    },
+};
+</script>
+
+<style scoped>
+.game-img {
+    width: 65%;
+    display: flex;
+    border-radius: 15px;
+    opacity: 0.7;
+}
+
+.game-img.active {
+    opacity: 1;
+    -webkit-box-shadow: 0 0 25px #591fe7;
+    box-shadow: 0 0 25px #591fe7;
+    border-width: 5px;
+    border: red;
+}
+
+.game-img:hover {
+    transform: scale(1.03);
+    transition: 0.3s;
+    cursor: pointer;
+}
+</style>
--- vue-Steam-Better-RecSys/src/components/MainCard.vue
+++ vue-Steam-Better-RecSys/src/components/MainCard.vue
@@ -3,14 +3,10 @@
         <div class="main-wrap w-100 h-100 pa-8 d-flex flex-column align-center">
             <div class="grid-container">
                 <div class="game-wrap" v-for="game in games">
-                    <img
-                        class="game-img"
-                        :src="game.verticalImageUrl"
-                        :alt="game.title"
+                    <GameCard
+                        :title="game.title"
+                        :image="game.verticalImageUrl"
                         :id="game.id"
-                        @click="setActive(game.id)"
-                        :class="{ active: isActive }"
-                        @error="setImage($event.currentTarget)"
                     />
                 </div>
             </div>
@@ -23,28 +19,18 @@
 import { mapActions, mapState } from 'pinia';
 import useGamesStore from '@/stores/games';
 import Pagination from '@/components/UI/Pagination.vue';
+import GameCard from './GameCard.vue';
 
 export default {
     name: 'mainCard',
     components: {
+        GameCard,
         Pagination,
-    },
-    data() {
-        return {
-            isActive: false,
-        };
     },
     methods: {
         ...mapActions(useGamesStore, ['getAllGamesStore', 'getFilteredGames']),
         async render() {
             await this.getAllGamesStore();
-        },
-        setImage(image) {
-            image.setAttribute('src', 'src/assets/Image404.png');
-        },
-
-        setActive(id) {
-            this.isActive = !this.isActive;
         },
     },
     computed: {
@@ -74,24 +60,5 @@ div {
     justify-content: center;
     flex-wrap: wrap;
     height: max-content;
-}
-
-.game-img {
-    width: 65%;
-    display: flex;
-    border-radius: 15px;
-    opacity: 0.7;
-}
-
-.game-img.active {
-    opacity: 1;
-    -webkit-box-shadow: 0 0 25px #591fe7;
-    box-shadow: 0 0 25px #591fe7;
-}
-
-.game-img:hover {
-    transform: scale(1.03);
-    transition: 0.3s;
-    cursor: pointer;
 }
 </style>
