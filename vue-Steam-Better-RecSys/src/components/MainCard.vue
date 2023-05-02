4deee5b321c780ffd94ed7264a90150b93471c97
eispoohw <darina.kormanovskaya@gmail.com>
2023-05-02 22:33:25 +0300

    :hammer: [refactor] main & game card

--- vue-Steam-Better-RecSys/src/components/MainCard.vue
+++ vue-Steam-Better-RecSys/src/components/MainCard.vue
@@ -1,25 +1,21 @@
 <template>
-    <div class="">
-        <div class="main-wrap w-100 h-100 pa-8 d-flex flex-column align-center">
-            <div class="grid-container">
-                <div class="game-wrap" v-for="game in games">
-                    <GameCard
-                        :title="game.title"
-                        :image="game.verticalImageUrl"
-                        :id="game.id"
-                    />
-                </div>
-            </div>
-            <Pagination />
+    <div class="d-flex flex-wrap justify-content-center">
+        <div class="mb-3 mx-3" v-for="game in games">
+            <GameCard
+                :title="game.title"
+                :image="game.verticalImageUrl"
+                :id="game.id"
+            />
         </div>
     </div>
+    <Pagination />
 </template>
 
 <script>
 import { mapActions, mapState } from 'pinia';
 import useGamesStore from '@/stores/games';
 import Pagination from '@/components/UI/Pagination.vue';
-import GameCard from './GameCard.vue';
+import GameCard from '@/components/GameCard.vue';
 
 export default {
     name: 'mainCard',
@@ -42,23 +38,4 @@ export default {
 };
 </script>
 
-<style scoped>
-div {
-    height: max-content;
-}
-
-.grid-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    row-gap: 50px;
-    margin-bottom: 30px;
-}
-
-.game-wrap {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    height: max-content;
-}
-</style>
+<style scoped></style>
