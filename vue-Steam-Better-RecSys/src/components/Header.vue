4dfe05ab07dbb87445bd68650e12cd73ae493daa
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-05-05 18:56:37 +0300

    :sparkles: [add] go home on logo click

--- vue-Steam-Better-RecSys/src/components/Header.vue
+++ vue-Steam-Better-RecSys/src/components/Header.vue
@@ -1,12 +1,21 @@
 <template>
     <header class="header sticky-top p-1 text-center">
-        <h1><b>Steam Wizzard</b><sup><small>β</small></sup></h1>
+        <div @click="goHome()">
+            <h1><b>Steam Wizzard</b><sup><small>β</small></sup></h1>
+        </div>
     </header>
 </template>
 
 <script>
+import router from "../router";
+
 export default {
     name: 'Header',
+    methods: {
+        goHome() {
+            router.push({ path: '/', replace: true })
+        }
+    }
 };
 </script>
 
@@ -15,5 +24,9 @@ header {
     background-color: var(--additional-bg-color);
     font-family: 'Beautiful Magic', sans-serif;
     color: var(--main-theme-color);
+}
+
+div {
+    cursor: pointer;
 }
 </style>
