fe45b34fc84e4a5e4c8138a565569fc11e68c364
eispoohw <darina.kormanovskaya@gmail.com>
2023-05-11 13:24:48 +0300

    :sparkles: [update] footer, add links for about and cookies pages

--- vue-Steam-Better-RecSys/src/components/Footer.vue
+++ vue-Steam-Better-RecSys/src/components/Footer.vue
@@ -1,22 +1,38 @@
 <template>
-    <footer class="mt-auto">
-        <div class="text-center py-2">
-            <p>Something wrong/broken/stupid?</p>
-            <p>Contact evlko dot oklve at gmail</p>
+    <footer class="mt-auto d-flex flex-column align-items-center py-1">
+        <div class="text-center my-0">
+            <p>Something wrong or broken? Contact evlko.oklve [at] gmail [dot] com</p>
+        </div>
+        <div class="col-12 col-sm-2 col-md-6 col-lg-4 d-flex flex-row my-0">
+            <div class="col-4">
+                <p class="text-center"><small><a>Privacy Polciy</a></small></p>
+            </div>
+            <div class="col-4">
+                <p class="text-center"><small><a href="/about">About</a></small></p>
+            </div>
+            <div class="col-4">
+                <p class="text-center"><small><a href="/cookies">Cookies</a></small></p>
+            </div>
         </div>
     </footer>
 </template>
 
 <script>
-export default {};
+export default {
+};
 </script>
 
 <style scoped>
+@import '../styles/main.css';
+
 footer {
     background: var(--additional-bg-color);
     color: var(--main-text-color);
 }
 p {
     margin-bottom: 0;
+}
+a {
+    color: var(--main-text-color);
 }
 </style>
