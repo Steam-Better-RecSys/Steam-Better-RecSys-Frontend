d117a8e7f620a13a353189392c85889127955401
eispoohw <darina.kormanovskaya@gmail.com>
2023-05-11 12:17:45 +0300

    :hammer: [fix] search query

--- vue-Steam-Better-RecSys/src/api/games.js
+++ vue-Steam-Better-RecSys/src/api/games.js
@@ -6,7 +6,9 @@ class GamesAPI {
     getFilteredGames = async (sort, tags, searchString, offset) => {
         const params = new URLSearchParams();
 
-        params.append('search', searchString)
+        if (searchString !== null) {
+            params.append('search', searchString)
+        }
         params.append('offset', offset);
 
         for (const elem of sort) {
