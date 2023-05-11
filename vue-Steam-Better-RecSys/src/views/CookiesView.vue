a02b5f88c15f4473b061ac19107ff50e36c7c340
eispoohw <darina.kormanovskaya@gmail.com>
2023-05-11 23:41:59 +0300

    :hammer: [fix] code style

--- vue-Steam-Better-RecSys/src/views/CookiesView.vue
+++ vue-Steam-Better-RecSys/src/views/CookiesView.vue
@@ -9,23 +9,26 @@
                 you use to access SteamWizzard, which enables features and
                 functionality. It allows our website to “remember” your activity
                 or preferences over a length of time or during a particular
-                session. If you're signed in to SteamWizzard, cookies allow us to
-                show you the right information and personalize your experience.
-                Usage of a cookie is in no way linked to any personally identifiable information on our site.
+                session. If you're signed in to SteamWizzard, cookies allow us
+                to show you the right information and personalize your
+                experience. Usage of a cookie is in no way linked to any
+                personally identifiable information on our site.
             </p>
             <h2>First-Party Cookies</h2>
             <p>
-                Cookies which are set by us (SteamWizzard) to facilitate
-                our provision of the service are known as First Party
-                Cookies.
+                Cookies which are set by us (SteamWizzard) to facilitate our
+                provision of the service are known as First Party Cookies.
             </p>
             <h2>Third-Party Cookies</h2>
             <p>We do not use third-party cookies.</p>
             <h2>Managing and refusing the use of cookies</h2>
             <p>
-                When you first visit our site, you will see a banner asking whether you would like to accept the use of cookies.
-                By clicking accepting the use of cookies you agree to the use of all cookies as described here.
-                Unfortunately, you cannot discard cookies because they are used in recommendations. Otherwise, everyone would have to register.
+                When you first visit our site, you will see a banner asking
+                whether you would like to accept the use of cookies. By clicking
+                accepting the use of cookies you agree to the use of all cookies
+                as described here. Unfortunately, you cannot discard cookies
+                because they are used in recommendations. Otherwise, everyone
+                would have to register.
             </p>
             <h2>Changes to this policy</h2>
             <p>
