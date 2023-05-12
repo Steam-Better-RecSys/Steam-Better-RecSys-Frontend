0a1a062ae249eb8767238a0cc0ae86dc66aa80c6
eispoohw <darina.kormanovskaya@gmail.com>
2023-05-12 19:32:10 +0300

    :hammer: [fix] filters mobile layout

--- vue-Steam-Better-RecSys/src/components/FiltersMenu.vue
+++ vue-Steam-Better-RecSys/src/components/FiltersMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="px-4 py-2">
-        <div class="row mb-2">
-            <div class="col-6">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6 mb-2">
                 <div class="input-group">
                     <input
                         type="text"
@@ -13,7 +13,7 @@
                     />
                 </div>
             </div>
-            <div  class="col-6">
+            <div  class="col-12 col-sm-12 col-md-6 mb-2">
                 <div class="input-group">
                     <input
                         type="text"
